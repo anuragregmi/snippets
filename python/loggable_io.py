ec1274ff6bef915574a6fd98206e66fd9da1b9f6
Anurag Regmi <anurag1234565@gmail.com>
2021-08-19 17:08:16 +0545

    Create loggable_io.py

---
+++ python/loggable_io.py
@@ -0,0 +1,71 @@
+import io
+import sys
+import contextlib
+
+from asgiref.sync import sync_to_async
+
+
+class Loggable:
+    """Interface for classes that will be passed in LoggableIO"""
+
+    def save_log(self, s: str) -> None:
+        """
+        Implement logic to save logs
+
+        Args:
+            s: Logged string
+        """
+        raise NotImplementedError
+
+
+class LoggableIO(io.TextIOBase):
+    """Loggable Stream, calls save_log of object passed when write is called.
+
+    Attributes:
+        loggable_object: Loggable instance to which logs will be associated to
+        terminal: variable holding value of sys.stdout
+        pass_through: Flag, when set will pass to stdout as well
+
+
+    Examples:
+
+        from contextlib import redirect_stdout
+
+        server = Server.objects.get(id=1)
+
+        with redirect_stdout(DatabaseAndStdOutIO(server=server)):
+            print("hello world")
+
+    """
+
+    def __init__(self, loggable_object: Loggable, pass_through: bool = True) -> None:
+        """Inits LoggableIO
+
+        Args:
+            loggable_object: Loggable instance to which logs will be associated to
+            pass_through: Flag, when set will pass to stdout as well
+        """
+        self.terminal: io.TextIO = sys.stdout
+        self.loggable_object = loggable_object
+        self.pass_through = pass_through
+
+    def write(self, s: str) -> int:
+        """Write the string s to the stream
+
+        Will call loggable_object.save_log(s), pass to stdout if set and also
+        to the stream
+
+        Args:
+            String to write
+
+        Returns:
+            the number of characters written
+        """
+        with contextlib.redirect_stdout(self.terminal):
+            # to avoid infinite recursion output of this is stdout
+            self.loggable_object.save_log(s)
+
+        if self.pass_through:
+            return self.terminal.write(s)
+
+        return len(s)
